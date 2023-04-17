13f79e3beae4c8af064cef77a261416b8ad43635
Dennis <126710731+GatoEstunado@users.noreply.github.com>
2023-04-17 20:34:12 -0300

    Update

--- Calcular dois Triangulos.cpp
+++ Calcular dois Triangulos.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 using namespace std;
 
-//
+// Adicionando as variaveis e desenvolvendo a conta a ser feita
 double calcularAreaTriangulo (double a, double b, double c) {
     double p = (a + b + c) / 2;
     return sqrt (p* (p -a) * (p - b) * (p - c));
@@ -11,29 +11,29 @@ double calcularAreaTriangulo (double a, double b, double c) {
 int main () {
     double xa, xb, xc, ya, yb, yc;
     
-    //
-    cout << "Digite as medidas dos lados do triangulo X (separados por espaço): " << endl;
+    // Interface do usuario e recebimento dos valores de X
+    cout << "Digite as medidas dos lados do triangulo X (separados por espaco): " << endl;
     cin >> xa >> xb >> xc;
     
-    //
-    cout << "Digite as medidas dos lados do triangulo Y (separados por espaço): " << endl;
+    // Interface do usuario de recebimento dos valores de Y
+    cout << "Digite as medidas dos lados do triangulo Y (separados por espaco): " << endl;
     cin >> ya >> yb >> yc;
     
-    //
+    // Introduzindo a "Area" e realizando o calculo
     double areaX = calcularAreaTriangulo (xa, xb, xc);
     double areaY = calcularAreaTriangulo (ya, yb, yc);
     
-    //
-    cout << "Área do triangulo X: " << areaX << endl;
-    cout << "Área do triangulo Y: " << areaY << endl;
+    // Entrega dos valores das areas para o usuario
+    cout << "Area do triangulo X: " << areaX << endl;
+    cout << "Area do triangulo Y: " << areaY << endl;
     
-    //
+    // Condicao If/Else para derteminar a maior area, ou se sao iguais
     if (areaX > areaY) {
-        cout << "O triangulo X tem a maior área." << endl;
+        cout << "O triangulo X tem a maior area." << endl;
     } else if (areaX < areaY) {
-        cout << "O triangulo Y tem a maior área." << endl;
+        cout << "O triangulo Y tem a maior area." << endl;
     } else {
-        cout << "Os triangulos tem a mesma área." << endl;
+        cout << "Os triangulos tem a mesma area." << endl;
     }
     return 0;
 } 
